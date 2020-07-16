ea27971303bf9ef27273d93eb42adf4c661c2620
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-07-17 00:38:16 +0700

    :whale: fix action

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-git pull
-docker build -t .
+git pull origin master
+docker-compose build
+docker-compose up --no-deps -d --build
+docker-compose exec -T backend yarn prisma-deploy
+docker-compose restart nginx
