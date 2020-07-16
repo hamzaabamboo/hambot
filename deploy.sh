e5ac2a34620a3ef848f81ce20ecd848dd6ea5499
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-07-17 00:59:05 +0700

    :wrench: fix command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-git pull origin master
+GIT_SSH_COMMAND="ssh -i ~/hambot_keys/github_key" git pull origin master
 docker-compose build
 docker-compose up --no-deps -d --build
 docker-compose exec -T backend yarn prisma-deploy
