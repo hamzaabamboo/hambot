6c6bf0b9e41e8a5cc1e1114e6371764213867d1e
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-07-17 10:30:09 +0700

    :wrench:

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-export GIT_SSH_COMMAND="ssh -i ~/hambot_keys/github_key" git pull origin master
+git pull origin master
 docker-compose build
 docker-compose up --no-deps -d --build
 docker-compose restart backend
