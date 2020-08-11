ea8f54b782a4c9e0fc151adcd2339994fa698663
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-08-11 16:08:33 +0700

    :sparkles: add weighted random

--- src/modules/commands/random.command.ts
+++ src/modules/commands/random.command.ts
@@ -1,25 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { BaseCommand } from './command.base';
 import { Message } from '../messages/messages.model';
-import { clone } from 'lodash';
+import { clone, max } from 'lodash';
 
 @Injectable()
 export class RandomCommand extends BaseCommand {
-  public command = /^random(?:(?: (group))?(?: (\d+))?(?: (.*))?)?/i;
+  public command = /^random(?:(?: (group|weighted))?(?: (\d+))?(?: (.*))?)?/i;
 
   async handle(message: Message, command: string, n = '2', things: string) {
     const stuff = things.split(' ');
+    const groups = Array(isNaN(Number(n)) || Number(n) < 1 ? 2 : Number(n))
+      .fill(undefined)
+      .map(_ => []);
     switch (command) {
       case 'group':
-        const groups = Array(isNaN(Number(n)) ? 2 : Number(n))
-          .fill(undefined)
-          .map(_ => []);
         const arr: string[] = clone(stuff);
         let i = 0;
         while (arr.length > 0) {
           const idx = Math.floor(Math.random() * arr.length);
           groups[i].push(arr.splice(idx, 1)[0]);
           i = (i + 1) % groups.length;
+        }
+        return {
+          ...message,
+          files: [],
+          message: groups
+            .map((e, i) => `Group ${i + 1}\n ${e.join(' ,')}`)
+            .join('\n'),
+        };
+      case 'weighted':
+        const names = stuff
+          .map(e => e.split(':'))
+          .map(([name, weight]) => ({
+            name,
+            weight: !isNaN(Number(weight)) ? Number(weight) : 1,
+          }));
+        const acc: { name: string; weight: number }[] = clone(names);
+        let group = 0;
+        while (acc.length > 0) {
+          const toDel = acc.reduce(
+            (p, c) =>
+              p.weight * Math.random() > c.weight * Math.random() ? p : c,
+            acc[0],
+          );
+          const idx = acc.findIndex(p => p.name === toDel.name);
+          groups[group].push(acc.splice(idx, 1)[0].name);
+          group = (group + 1) % groups.length;
         }
         return {
           ...message,
