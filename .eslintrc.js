dc88687fbfc31a67a8f87d8476183d20b7d5a74d
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-09-28 23:35:29 +0700

    :rotating_light: fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-misused-promises': 'error',
+    "@typescript-eslint/camelcase": "off"
   },
 };
