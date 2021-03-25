e7e284f58ff4141a397daeef1663d385a0f59848
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-03-25 13:17:55 +0700

    :sparkles: CORS

--- src/modules/d4dj/d4dj.controller.ts
+++ src/modules/d4dj/d4dj.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, HttpService, Query } from '@nestjs/common';
+import { Controller, Get, Header, HttpService, Query } from '@nestjs/common';
 
 @Controller('/d4dj')
 export class D4DJController {
   constructor(private http: HttpService) {}
 
   @Get('/sig')
+  @Header('Access-Control-Allow-Origin', 'hamzaabamboo.github.io')
+  @Header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  @Header('Access-Control-Allow-Headers', 'Content-Type')
   async getHello(@Query('url') url: string) {
     if (!url || !url.match('projectdivar')) return ':P';
     const res = await this.http.get(url).toPromise();
