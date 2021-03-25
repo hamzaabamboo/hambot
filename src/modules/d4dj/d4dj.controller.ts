1be56e1ec2ad5994ccbe536b50a4a4c9975ac6d0
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-03-25 13:25:46 +0700

    :bug: wrong cors header oops

--- src/modules/d4dj/d4dj.controller.ts
+++ src/modules/d4dj/d4dj.controller.ts
@@ -5,7 +5,7 @@ export class D4DJController {
   constructor(private http: HttpService) {}
 
   @Get('/sig')
-  @Header('Access-Control-Allow-Origin', 'hamzaabamboo.github.io')
+  @Header('Access-Control-Allow-Origin', 'https://hamzaabamboo.github.io')
   @Header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   @Header('Access-Control-Allow-Headers', 'Content-Type')
   async getHello(@Query('url') url: string) {
