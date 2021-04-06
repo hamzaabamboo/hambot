4fcec4f761cdb6053617870b413a778f3dc6ac46
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-04-06 15:34:56 +0700

    :sparkles: new proxy

--- src/modules/d4dj/d4dj.controller.ts
+++ src/modules/d4dj/d4dj.controller.ts
@@ -7,40 +7,60 @@ import {
   Post,
   Query,
   Req,
+  Res,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { request } from 'gaxios';
+import { Readable } from 'stream';
 
 @Controller('/d4dj')
 export class D4DJController {
   constructor(private http: HttpService) {}
 
   @Get('/sig')
+  @Post('/sig')
   @Header('Access-Control-Allow-Origin', 'https://hamzaabamboo.github.io')
   @Header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   @Header('Access-Control-Allow-Headers', 'Content-Type')
-  async proxySig(@Query('url') url: string) {
+  async proxySig(
+    @Query('url') url: string,
+    @Res() res: Response<any, any>,
+    @Req() req: Request<any>,
+  ) {
     if (!url || !url.match('projectdivar')) return ':P';
-    const res = await this.http.get(url).toPromise();
-    return res.data;
+    console.log(url);
+    const a = await request<Readable>({
+      url: url as string,
+      method: req.method ?? ('GET' as any),
+      data: req.body,
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+      responseType: 'stream',
+    });
+    res.send(a.data);
   }
 
   @Get('/d4db')
-  @Header('Access-Control-Allow-Origin', 'https://hamzaabamboo.github.io')
-  @Header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  @Header('Access-Control-Allow-Headers', 'Content-Type')
-  async getD4DB(@Query('url') url: string) {
-    if (!url || !url.match('d4-dj')) return ':P';
-    const res = await this.http.get(url).toPromise();
-    return res.data;
-  }
-
   @Post('/d4db')
   @Header('Access-Control-Allow-Origin', 'https://hamzaabamboo.github.io')
   @Header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   @Header('Access-Control-Allow-Headers', 'Content-Type')
-  async postD4DB(@Query('url') url: string, @Body() body: any) {
+  async getD4DB(
+    @Query('url') url: string,
+    @Res() res: Response<any, any>,
+    @Req() req: Request<any>,
+  ) {
     if (!url || !url.match('d4-dj')) return ':P';
-    const res = await this.http.post(url, body).toPromise();
-    return res.data;
+    const a = await request<Readable>({
+      url: req.query.url as string,
+      method: req.method ?? ('GET' as any),
+      data: req.body,
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+      responseType: 'stream',
+    });
+    res.send(a.data);
   }
 }
