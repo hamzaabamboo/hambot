f14d0bd8b51a6ba789aaa3acb0997b46fbbc28e9
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2022-10-16 12:52:34 +0700

    :sparkles: upgrade

--- src/modules/covid/covid.controller.ts
+++ src/modules/covid/covid.controller.ts
@@ -12,14 +12,16 @@ export class CovidController {
   constructor(private covid: CovidService) {}
   @Get('summary')
   @Header('content-type', 'image/png')
-  @Header('content-disposition', 'attachment; filename=qrcode.png')
+  // @Header('content-disposition', 'attachment; filename=qrcode.png')
   async getCovidSummary() {
     const data = await this.covid.getCovidData();
+    console.log(data);
     const canvas = createCanvas(WIDTH, HEIGHT);
     const ctx = canvas.getContext('2d');
     let currentHeight = PADDING;
     ctx.fillStyle = 'red';
     currentHeight = HEIGHT / 2;
+    // TOP LINE
     ctx.fillRect(
       PADDING,
       PADDING,
@@ -34,82 +36,101 @@ export class CovidController {
     printTextInTheMiddle(
       WIDTH / 2,
       (3 * HEIGHT) / 8,
-      `(+${data.NewConfirmed})`,
+      `(${printNumberWithSign(data.NewConfirmed)})`,
       ctx,
     );
+
+    // Second Line
     ctx.fillStyle = 'green';
     ctx.fillRect(
       PADDING,
       HEIGHT / 2 + PADDING,
-      WIDTH / 3 - 2 * PADDING,
+      (data.Hospitalized ? WIDTH / 3 : WIDTH / 2) - 2 * PADDING,
       HEIGHT / 2 - 2 * PADDING,
     );
     ctx.font = '24px Noto';
-    printTextInTheMiddle(WIDTH / 6, (5 * HEIGHT) / 8, `Recovered`, ctx);
+    printTextInTheMiddle(
+      data.Hospitalized ? WIDTH / 6 : WIDTH / 4,
+      (5 * HEIGHT) / 8,
+      `Recovered`,
+      ctx,
+    );
     ctx.font = 'bold 40px Noto';
     printTextInTheMiddle(
-      WIDTH / 6,
+      data.Hospitalized ? WIDTH / 6 : WIDTH / 4,
       (3 * HEIGHT) / 4,
       String(data.Recovered),
       ctx,
     );
     ctx.font = '30px Noto';
     printTextInTheMiddle(
-      WIDTH / 6,
+      data.Hospitalized ? WIDTH / 6 : WIDTH / 4,
       (7 * HEIGHT) / 8,
-      `(+${data.NewRecovered})`,
+      `(${printNumberWithSign(data.NewRecovered)})`,
       ctx,
     );
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(
-      WIDTH / 3 + PADDING,
-      HEIGHT / 2 + PADDING,
-      WIDTH / 3 - 2 * PADDING,
-      HEIGHT / 2 - 2 * PADDING,
-    );
-    ctx.font = '24px Noto';
-    printTextInTheMiddle(WIDTH / 2, (5 * HEIGHT) / 8, `Hospitalized`, ctx);
-    ctx.font = 'bold 40px Noto';
-    printTextInTheMiddle(
-      WIDTH / 2,
-      (3 * HEIGHT) / 4,
-      String(data.Hospitalized),
-      ctx,
-    );
-    ctx.font = '30px Noto';
-    printTextInTheMiddle(
-      WIDTH / 2,
-      (7 * HEIGHT) / 8,
-      `(+${data.NewHospitalized})`,
-      ctx,
-    );
+
+    if (data.Hospitalized) {
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(
+        WIDTH / 3 + PADDING,
+        HEIGHT / 2 + PADDING,
+        WIDTH / 3 - 2 * PADDING,
+        HEIGHT / 2 - 2 * PADDING,
+      );
+      ctx.font = '24px Noto';
+      printTextInTheMiddle(WIDTH / 2, (5 * HEIGHT) / 8, `Hospitalized`, ctx);
+      ctx.font = 'bold 40px Noto';
+      printTextInTheMiddle(
+        WIDTH / 2,
+        (3 * HEIGHT) / 4,
+        String(data.Hospitalized),
+        ctx,
+      );
+      ctx.font = '30px Noto';
+      printTextInTheMiddle(
+        WIDTH / 2,
+        (7 * HEIGHT) / 8,
+        `(${printNumberWithSign(data.NewHospitalized)})`,
+        ctx,
+      );
+    }
     ctx.fillStyle = 'grey';
     ctx.fillRect(
-      (2 * WIDTH) / 3 + PADDING,
+      (data.Hospitalized ? (2 * WIDTH) / 3 : WIDTH / 2) + PADDING,
       HEIGHT / 2 + PADDING,
-      WIDTH / 3 - 2 * PADDING,
+      (data.Hospitalized ? WIDTH / 3 : WIDTH / 2) - 2 * PADDING,
       HEIGHT / 2 - 2 * PADDING,
     );
     ctx.font = '24px Noto';
-    printTextInTheMiddle((5 * WIDTH) / 6, (5 * HEIGHT) / 8, `Death`, ctx);
+    printTextInTheMiddle(
+      data.Hospitalized ? (5 * WIDTH) / 6 : (3 * WIDTH) / 4,
+      (5 * HEIGHT) / 8,
+      `Death`,
+      ctx,
+    );
     ctx.font = 'bold 40px Noto';
     printTextInTheMiddle(
-      (5 * WIDTH) / 6,
+      data.Hospitalized ? (5 * WIDTH) / 6 : (3 * WIDTH) / 4,
       (3 * HEIGHT) / 4,
       String(data.Deaths),
       ctx,
     );
     ctx.font = '30px Noto';
     printTextInTheMiddle(
-      (5 * WIDTH) / 6,
+      data.Hospitalized ? (5 * WIDTH) / 6 : (3 * WIDTH) / 4,
       (7 * HEIGHT) / 8,
-      `(+${data.NewDeaths})`,
+      `(${printNumberWithSign(data.NewDeaths)})`,
       ctx,
     );
 
     return canvas.createPNGStream();
   }
 }
+
+const printNumberWithSign = (number: number) => {
+  return number >= 0 ? `+${number}` : number;
+};
 
 const printTextInTheMiddle = (
   w: number,
--- src/modules/covid/covid.service.ts
+++ src/modules/covid/covid.service.ts
@@ -6,21 +6,65 @@ import moment, { Moment } from 'moment';
 import { join } from 'path';
 import { Message } from '../messages/messages.model';
 import { PushService } from '../push/push.service';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 
 interface COVIDAPIResult {
   Confirmed: number;
   Recovered: number;
-  Hospitalized: number;
+  Hospitalized?: number;
   Deaths: number;
   NewConfirmed: number;
   NewRecovered: number;
-  NewHospitalized: number;
+  NewHospitalized?: number;
   NewDeaths: number;
   UpdateDate: string;
 }
 
+type MOPHAPIResult = {
+  year: number;
+  weeknum: number;
+  new_case: number;
+  total_case: number;
+  new_case_excludeabroad: number;
+  total_case_excludeabroad: number;
+  new_recovered: number;
+  total_recovered: number;
+  new_death: number;
+  total_death: number;
+  case_foreign: number;
+  case_prison: number;
+  case_walkin: number;
+  case_new_prev: number;
+  case_new_diff: number;
+  death_new_prev: number;
+  death_new_diff: number;
+  update_date: string;
+}[];
+
+const parseAPI = (result: MOPHAPIResult): COVIDAPIResult => {
+  const {
+    total_case,
+    total_recovered,
+    total_death,
+    new_recovered,
+    case_new_diff,
+    death_new_diff,
+    update_date,
+  } = result[0];
+  return {
+    Confirmed: total_case,
+    Recovered: total_recovered,
+    Deaths: total_death,
+    NewConfirmed: case_new_diff,
+    NewRecovered: new_recovered,
+    NewDeaths: death_new_diff,
+    UpdateDate: update_date,
+  };
+};
+
 const GET_COVID_URL =
-  'https://static.easysunday.com/covid-19/getTodayCases.json';
+  'https://covid19.ddc.moph.go.th/api/Cases/today-cases-all';
+// 'https://static.easysunday.com/covid-19/getTodayCases.json';
 @Injectable()
 export class CovidService {
   lastUpdated: Moment;
@@ -48,7 +92,10 @@ export class CovidService {
       )} 
 ผู้ติดเชื่อ: ${data.Confirmed} (+${data.NewConfirmed})
 หายแล้ว: ${data.Recovered} (+${data.NewRecovered})
-รักษาอยู่ในรพ.: ${data.Hospitalized} (+${data.NewHospitalized})
+${
+  data.Hospitalized &&
+  `รักษาอยู่ในรพ.: ${data.Hospitalized} (+${data.NewHospitalized})`
+}
 เสียชีวิต: ${data.Deaths} (+${data.NewDeaths})
       `;
     });
@@ -56,10 +103,10 @@ export class CovidService {
   }
 
   async getCovidData() {
-    const { data } = await this.http
-      .get<COVIDAPIResult>(GET_COVID_URL)
-      .toPromise();
-    return data;
+    const { data } = await firstValueFrom(
+      this.http.get<MOPHAPIResult>(GET_COVID_URL),
+    );
+    return parseAPI(data);
   }
 
   generateCovidMessageToScarePeople(
