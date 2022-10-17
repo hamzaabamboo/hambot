4e62fa18ca696d6c6a70fbd216ea1db1edaefd20
aihuamen <wasuthon@amity.co>
2022-10-17 22:47:17 +0700

    feat: add random best friend

--- src/modules/commands/friend.command.ts
+++ src/modules/commands/friend.command.ts
@@ -5,16 +5,25 @@ import { BaseCommand } from './command.base';
 @Injectable()
 export class FriendCommand extends BaseCommand {
   public command = /^(friend)/i;
+  public bestFriends = ['Ham-san', 'หิม', 'Yama'];
   public quotes = [
     'ไม่ได้มึงนี่กูแย่เลย',
     'I am not a noodle guy',
-    'Mr. Ham, I saw you at MBK. Are you still there?',
+    'Mr. {{NAME}}, I saw you at MBK. Are you still there?',
   ];
 
   async handle(message: Message) {
+    const selectedIndex = Math.floor(Math.random() * this.quotes.length);
+    let selectedQuote = this.quotes[selectedIndex];
+    if (selectedIndex === 2) {
+      const realBestFriend = this.bestFriends[
+        Math.floor(Math.random() * this.bestFriends.length)
+      ];
+      selectedQuote = selectedQuote.replace('{{NAME}}', realBestFriend);
+    }
     return {
       files: [],
-      message: this.quotes[Math.floor(Math.random() * this.quotes.length)],
+      message: selectedQuote,
     };
   }
 }
