68b691eeea174ee1694534dfbb688bd5d4a79aff
himkwtn <him10424@gmail.com>
2022-10-24 15:11:43 +0700

    fix: fix di

--- src/config/app-config.module.ts
+++ src/config/app-config.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppConfigService } from './app-config.service';
 
 @Global()
-@Module({ providers: [AppConfigService], exports: [AppConfigService] })
+@Module({ imports: [ConfigModule], providers: [AppConfigService], exports: [AppConfigService] })
 export class AppConfigModule { }
